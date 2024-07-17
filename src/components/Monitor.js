1b30fc8e8cb3f00cf677656e92ab559d3f74b1b4
Tianshu <tian-shu@sjtu.edu.cn>
2024-07-17 19:35:57 +0800

    change monitor format

--- src/components/Monitor.js
+++ src/components/Monitor.js
@@ -7,7 +7,7 @@ function Monitor(props) {
     const fetchData = async () => {
         const mcu_url = "http://10.42.0.1:8080/monitor";
         const local_url = "http://localhost:8080/monitor";
-        const response = await fetch(local_url);
+        const response = await fetch(mcu_url);
         const data = await response.json();
         console.log('data', data)
         setMonitorData(data);
@@ -21,13 +21,43 @@ function Monitor(props) {
     }, []);
     
     console.log('monitor data', monitorData);
+    if (!monitorData || monitorData.error) {
+        return (
+            <div className="monitorTable">
+                <div className="monitorName">
+                    No available data 
+                </div>
+                <div className="monitorName">
+                    Please turn on
+                </div>
+            </div>
+        )
+    }
     return (
         <div className="monitorTable">
             <div className="monitorName">
-                Power Data: 
+                Forward power: 
+                <div className="monitorData">
+                    {monitorData.forward_power}
+                </div>
+            </div>
+            <div className="monitorName">
+                Backward power: 
             </div>
             <div className="monitorData">
-                {monitorData && monitorData.power}
+                {monitorData.backward_power}
+            </div>
+            <div className="monitorName">
+                Voltage: 
+            </div>
+            <div className="monitorData">
+                {monitorData.voltage}
+            </div>
+            <div className="monitorName">
+                Current: 
+            </div>
+            <div className="monitorData">
+                {monitorData.current}
             </div>
         </div>
     );
