23af34f96c6e51da7f281641a5cfa07b9ffb01f8
Tianshu <tian-shu@sjtu.edu.cn>
2024-07-23 19:15:21 +0800

    change logo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -15,7 +15,7 @@ function Home() {
                     mission
                 </div>
             </div> */}
-            <Monitor />
+            {/* <Monitor /> */}
             <ToggleForm />
             <ToggleButton />
         </div>
